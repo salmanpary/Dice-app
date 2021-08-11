b86f26d8edae1245b2cf45e2ebf4dcf4ca90ad0a
salmanpary <salmanpary@gmail.com>
2021-08-12 00:52:57 +0530

    dice app basic level finished

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber=1;
+  int rightdicenumber=1;
+  void changediceface(){
+    setState(() {
+      leftdicenumber=Random().nextInt(6)+1;
+      rightdicenumber=Random().nextInt(6)+1;
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Center(
+      child: Row(
+
+        children:<Widget> [
+          Expanded(
+            child: TextButton(
+              onPressed:(){
+                setState(() {
+                  changediceface();
+
+                });
+
+              } ,
+              child: Image.asset('images/dice${leftdicenumber}.png'
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: TextButton(
+              onPressed:(){
+                setState(() {
+                  changediceface();
+                });
+              } ,
+              child: Image.asset('images/dice${rightdicenumber}.png'
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
